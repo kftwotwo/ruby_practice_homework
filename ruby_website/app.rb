ae5805c54df8a056139c7d08ba621987884f82c4
Kevin A. Finley <kf.two.two@gmail.com>
2016-11-12 18:03:36 -0800

    add form to app and file

--- ruby_website/app.rb
+++ ruby_website/app.rb
@@ -2,11 +2,21 @@ require('sinatra')
 require('sinatra/reloader')
 
 get('/') do
-  @friend = "Chance"
+  @recipient = "Chance"
   @sender = "Kevin"
   erb(:letter)
 end
 
 get('/page2') do
   erb(:second)
+end
+
+get("/greeting_form") do
+  erb(:form)
+end
+
+get("greeting_card") do
+  @sender = params.fetch('sender')
+  @recipient = params.fetch('recipient')
+  erb(:greeting_card)
 end
